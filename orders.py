86cb3555d274d2d73fb5803ecbe60704d08de198
Pedro Shimomoto <pedrokenzoshimomoto@gmail.com>
2026-08-24 20:21:44 -0300

    Adição de alguns fallbacks, int() e decimal(). Decimal por conta da imprecisão do float

--- orders.py
+++ orders.py
@@ -1,20 +1,37 @@
+from decimal import Decimal
 seq = 0
+orders_list = []
+
 while True:
     user_input = input(f'>>>')
     seq += 1
-    if 'limit' in user_input: #fun limit/market
-        splitted = user_input.split()
-        order = splitted[0] #da pra usar POO aqui
+    splitted = user_input.split()
+    
+    if not splitted:
+        continue
+
+    order = splitted[0].lower() #da pra usar POO aqui
+    
+    if order == 'limit': #fun limit/market
+        if len(splitted) != 4:
+            print(f'Argumentos faltando')
+            continue
+
         side = splitted[1]
-        price = splitted[2]
-        qty = splitted[3]
-        id = 'identificator_' + str(seq) #tem que ficar dentro, se der print book, vai contar como id
-        print(f'id: {id}')
-        
-    if 'market' in user_input:
-        splitted = user_input.split()
+        price = Decimal(splitted[2])
+        qty = int(splitted[3])
+
+    elif order == 'market':
+        if len(splitted) != 3:
+            print(f'Argumentos faltando')
+            continue
         order = splitted[0]
         side = splitted[1] # a primeiro momento não dar id pra market, pois assume-se que ela morre no instante do input
-        qty = splitted[2] #isso q muda do market pro limit
+        qty = int(splitted[2]) #isso q muda do market pro limit
+
+    else:
+        print(f'Comando desconhecido')
         
     print(f'order: {order}, side: {side}, qty: {qty}')
+
+
